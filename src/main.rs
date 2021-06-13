74967593e8e361b73a35e872aa28c16d69703a48
Иван Илляшенко <illyashenko@inbox.ru>
2021-06-13 09:11:24 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use appHttpMicroService::http::*;
-use appHttpMicroService::models::*;
-use appHttpMicroService::service::*;
 use actix_web::{App, HttpServer};
-use actix_web::dev::Server;
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
